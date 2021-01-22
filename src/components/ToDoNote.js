cb75097662f207439a1504a441a52f12efb9ae38
Jelena <47182797+JDugonjic@users.noreply.github.com>
2021-01-22 20:36:54 +0100

    Version 1.0.4-pink

--- src/components/ToDoNote.js
+++ src/components/ToDoNote.js
@@ -4,8 +4,17 @@ import Checkbox from "@material-ui/core/Checkbox";
 import { db } from "../firebase";
 import { IconButton } from "@material-ui/core";
 import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined";
+import { makeStyles } from "@material-ui/styles";
 
 const ToDoNote = forwardRef(({ id, note, checked }, ref) => {
+  const useStyles = makeStyles({
+    checkbox: {
+      color: "pink",
+    },
+  });
+
+  const classes = useStyles();
+
   const checkNote = () => {
     if (!checked) {
       db.collection("notes").doc(id).set(
@@ -30,7 +39,7 @@ const ToDoNote = forwardRef(({ id, note, checked }, ref) => {
       <Checkbox
         checked={checked}
         onChange={checkNote}
-        className={styles.checkbox}
+        className={classes.checkbox}
       />
       <h3 className={checked && styles.noteChecked}>{note}</h3>
       <IconButton onClick={deleteNote} className={styles.delete}>
