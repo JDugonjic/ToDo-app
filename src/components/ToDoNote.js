27d6f3d39c5db60eadb8080f03fdd076762ab87e
Jelena <47182797+JDugonjic@users.noreply.github.com>
2021-01-22 19:09:58 +0100

    Version 1.0.1

--- src/components/ToDoNote.js
+++ src/components/ToDoNote.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import styles from "../styles/ToDoNote.module.css";
 import Checkbox from "@material-ui/core/Checkbox";
 import { db } from "../firebase";
 import { IconButton } from "@material-ui/core";
 import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined";
 
-function ToDoNote({ id, note, checked }) {
+const ToDoNote = forwardRef(({ id, note, checked }, ref) => {
   const checkNote = () => {
     if (!checked) {
       db.collection("notes").doc(id).set(
@@ -26,7 +26,7 @@ function ToDoNote({ id, note, checked }) {
   };
 
   return (
-    <div className={styles.todonote}>
+    <div ref={ref} className={styles.todonote}>
       <Checkbox onChange={checkNote} className={styles.checkbox} />
       <h3 className={checked && styles.noteChecked}>{note}</h3>
       <IconButton onClick={deleteNote} className={styles.delete}>
@@ -34,6 +34,6 @@ function ToDoNote({ id, note, checked }) {
       </IconButton>
     </div>
   );
-}
+});
 
 export default ToDoNote;
