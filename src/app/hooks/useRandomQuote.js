37e502deff378eaf829fef707da8cd7c777891ae
Jelena <47182797+JDugonjic@users.noreply.github.com>
2021-05-31 12:01:10 +0200

    useRandomQuote hook fix

--- src/app/hooks/useRandomQuote.js
+++ src/app/hooks/useRandomQuote.js
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 function useRandomQuote() {
   const [quote, setQuote] = useState([null]);
 
-  const fetchQuotes = async () => {
-    return await fetch("https://type.fit/api/quotes")
+  const fetchQuotes = () => {
+    fetch('https://type.fit/api/quotes')
       .then((response) => response.json())
       .then((data) => {
         const randomNum = Math.floor(Math.random() * data.length + 1);
 
         setQuote(data[randomNum]);
       })
-      .catch((error) => setQuote("error"));
+      .catch((error) => setQuote('error'));
   };
 
   useEffect(() => {
